65fe2f38f161cd9128e4bce6e8920fd944f0502a
Niño Andrey H. Amboy <112390917+Amboy13@users.noreply.github.com>
2023-03-09 21:34:39 +0800

    Update User.kt
    Ey

--- app/src/main/java/com/example/gymhub_fitnessxhabittracker/User.kt
+++ app/src/main/java/com/example/gymhub_fitnessxhabittracker/User.kt
@@ -5,7 +5,7 @@ data class User(
     val email:String? = null, val fname:String? = null,
     val lname:String? = null, val bday:String? = null,
     val age:String? = null, val gender:String? = null,
-    val wt:String? = null, val ht:String? = null,
+    val wt:String? = null, val ht:String? = null, 
     val bmi: String? = null, val cal: String? = null){
 
 }
